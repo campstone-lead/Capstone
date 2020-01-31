a625364963e5f7f45204b8e4bc9cb59a5a939f4b
Emma Hartman <emmajhartman@gmail.com>
2020-01-31 13:04:41 -0500

    Feature: Booker API routes completed

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
+router.use('/bookers', require('./bookers'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
