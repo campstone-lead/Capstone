ec513c2937e0f338a963080cf8777a160a4c5e00
lianach <liana.andreea97@yhaoo.com>
2020-02-03 18:56:30 -0500

    added login

---
+++ .history/src/pages/BookerSignup5_20200203000830 2.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,IonSearchbar,IonItem,IonInput,IonLabel,IonButton,IonCard } from '@ionic/react';
+import './Tab1.css';
+import {connect} from 'react-redux'
+import './BookerSignup2.css';
+import{updatedVenue} from '../store/booker'
+interface IMyComponentState {
+ capacity:string
+}
+interface IMyComponentProps{
+  booker:object,
+  updateVenue:any
+}
+ class Login extends React.Component<IMyComponentProps,IMyComponentState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+     capacity:''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.updateVenue({capacity:Number(this.state.capacity)})
+
+  }
+  componentDidMount(){
+    // let booker=window.localStorage.getItem('booker')
+    // booker=JSON.parse(booker||'');
+    // let newBooker=booker||{};
+    // if(newBooker["venue"]["capacity"]!==undefined){
+    //   this.setState({
+    //     capacity:newBooker["venue"].capacity
+    //   })
+    // }
+  }
+  render() {
+  return (
+
+    <IonPage>
+      <IonHeader >
+        <IonToolbar id="bar" >
+          <IonTitle>Venue capacity</IonTitle>
+          {/* <IonSearchbar className="search" placeholder="Search for venue..."  color="red"/> */}
+        </IonToolbar>
+      </IonHeader>
+
+    <IonContent>
+    <IonCard className="welcome-card">
+      <form onSubmit={this.handleSubmit} >
+    <IonTitle>How many people can fit in your venue?</IonTitle>
+      <IonItem >
+
+        <IonInput type="text" required
+          value={this.state.capacity}
+          onIonChange={(e) => this.setState({capacity:(e.target as HTMLInputElement).value})}
+          placeholder="Capacity"
+          className={"description"}
+        />
+        </IonItem>
+
+
+        <IonButton type="submit"  >Submit</IonButton>
+        <IonButton href={'/home'}
+        disabled={(this.state.capacity.length===0)?true:false}
+        >Next</IonButton>
+        </form>
+
+      </IonCard>
+      </IonContent>
+    </IonPage>
+  )
+  }
+};
+const mapStateToProps=(state)=>({
+  booker:state.booker
+})
+const mapDispatchToProps=(dispatch)=>({
+  updateVenue:(data)=>dispatch(updatedVenue(data))
+})
+export default connect(mapStateToProps,mapDispatchToProps)(Login);
