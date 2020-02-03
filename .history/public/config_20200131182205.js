05b27b13a66c58e3ecca9fcc43636f0c4ce7e44c
lianach <liana.andreea97@yhaoo.com>
2020-02-02 20:54:09 -0500

    added google api and signup form

--- .history/public/config_20200131182205.js
+++
@@ -1,5 +0,0 @@
-
-
-const config = { // this should be const instead if you're using ES6 standards
-apiKey : "https://maps.googleapis.com/maps/api/js?key=  AIzaSyBN_IKIZLzMfhPcu28VhP8u_SslBKWh-iE&libraries=places&language=en"
-}
--- .history/public/config_20200131182308.js
+++
@@ -1,4 +0,0 @@
-
-
-const config = "https://maps.googleapis.com/maps/api/js?key=  AIzaSyBN_IKIZLzMfhPcu28VhP8u_SslBKWh-iE&libraries=places&language=en"
-
