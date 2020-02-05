f8995823668eef7b7907069cc9719723d12a636e
Ariana Hwang <ajhwang0717@gmail.com>
2020-02-05 17:25:26 -0500

    feature: artist password added

---
+++ src/pages/Artist/Signup/ArtistPassword.tsx
@@ -0,0 +1,84 @@
+import {
+    IonContent, IonHeader, IonPage, IonTitle, IonToolbar,IonItem,IonInput,IonLabel,IonButton, IonCard
+  } from '@ionic/react';
+  import React from 'react';
+  import '../../Tab1.css';
+  import {connect} from 'react-redux'
+  import {updatedArtist} from '../../../store/artist'
+
+  interface IMyComponentState {
+    password:string,
+  }
+
+  interface IMyComponentProps{
+    // putType: (artistType: any) => void,
+    updateArtist: any
+  }
+
+  class ArtistPassword extends React.Component<IMyComponentProps,IMyComponentState>  {
+    constructor(props){
+        super(props);
+        this.state = {
+            password: ''
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log(this.state)
+        this.props.updateArtist(this.state);
+    }
+    render() {
+
+        return (
+      
+          <IonPage>
+            <IonHeader >
+              <IonToolbar id="bar" >
+                <IonTitle>Personal information</IonTitle>
+              </IonToolbar>
+            </IonHeader>
+      
+          <IonContent>
+          <IonCard className="welcome-card">
+            <form onSubmit={this.handleSubmit}>
+            <IonTitle>Let's add a password...</IonTitle>
+      
+            <IonItem>
+      
+              <IonInput type="password" placeholder="Password" required
+                value={this.state.password}
+                onIonChange={(e) => this.setState({password:(e.target as HTMLInputElement).value})}
+              />
+              </IonItem>
+      
+      
+      <IonItem routerLink='/home' >
+      <br></br>
+      
+            <IonButton type="submit"  disabled={(this.state.password.length===0)?true:false}
+            onClick={()=>{
+              window.localStorage.clear();
+            }}
+            >Submit</IonButton>
+      </IonItem>
+      
+      
+              </form>
+      
+            </IonCard>
+            </IonContent>
+          </IonPage>
+        )
+    }
+}
+
+// const mapStateToProps = (state) => ({
+//     artist: state.artist
+// })
+
+const mapDispatchToProps = (dispatch) => ({
+    updateArtist: (artistInfo) => dispatch(updatedArtist(artistInfo))
+})
+
+export default connect(null, mapDispatchToProps)(ArtistPassword)
--- src/pages/Artist/Signup/UploadPicture.tsx
+++ src/pages/Artist/Signup/UploadPicture.tsx
@@ -1,24 +1,24 @@
-import { IonActionSheet, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonImg, IonFab, IonFabButton, IonIcon, IonItem, IonButton, IonAvatar} from '@ionic/react';
+import { IonActionSheet, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonImg, IonFab, IonFabButton, IonIcon, IonItem, IonButton, IonAvatar } from '@ionic/react';
 import React, { Component } from 'react';
-import { Plugins, CameraResultType, CameraSource} from '@capacitor/core';
-import {updatedArtist} from '../../../store/artist'
-import {connect} from 'react-redux'
+import { Plugins, CameraResultType, CameraSource } from '@capacitor/core';
+import { updatedArtist } from '../../../store/artist'
+import { connect } from 'react-redux'
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import { add } from 'ionicons/icons'
 const { Camera } = Plugins;
 
-const DEFAULT_PIC='https://images.vexels.com/media/users/3/147101/isolated/preview/b4a49d4b864c74bb73de63f080ad7930-instagram-profile-button-by-vexels.png'
-interface IMyComponentProps{
-  updateArtist:any
+const DEFAULT_PIC = 'https://images.vexels.com/media/users/3/147101/isolated/preview/b4a49d4b864c74bb73de63f080ad7930-instagram-profile-button-by-vexels.png'
+interface IMyComponentProps {
+  updateArtist: any
 }
 
 interface IMyComponentState {
-  photo: string
+  imageUrl: string
 }
-export class UploadPicture extends Component <IMyComponentProps,IMyComponentState>  {
+export class UploadPicture extends Component<IMyComponentProps, IMyComponentState>  {
   constructor(props: any) {
     super(props)
-    this.state = {photo: DEFAULT_PIC}
+    this.state = { imageUrl: DEFAULT_PIC }
     defineCustomElements(window)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
@@ -34,16 +34,16 @@ export class UploadPicture extends Component <IMyComponentProps,IMyComponentStat
   // }
   async takePicture() {
     const image = await Camera.getPhoto({
-    quality: 90,
-    allowEditing: false,
-    resultType: CameraResultType.Uri,
-    source: CameraSource.Photos
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Photos
     })
 
     var imageUrl = image.webPath;
     // Can be set to the src of an image now
     this.setState({
-    photo: imageUrl || DEFAULT_PIC
+      imageUrl: imageUrl || DEFAULT_PIC
     })
   }
   handleSubmit(event) {
@@ -52,7 +52,7 @@ export class UploadPicture extends Component <IMyComponentProps,IMyComponentStat
   }
 
   render() {
-    const { photo } = this.state;
+    const { imageUrl } = this.state;
     return (
       <IonPage>
         <IonHeader>
@@ -62,20 +62,20 @@ export class UploadPicture extends Component <IMyComponentProps,IMyComponentStat
         </IonHeader>
         <IonContent>
           <form onSubmit={this.handleSubmit}>
-          {/* <IonButton type='button'onClick={() => this.takePicture()}> add picture</IonButton> */}
-          <IonAvatar style={{'width':'370px','height':'370px',}} >\
+            {/* <IonButton type='button'onClick={() => this.takePicture()}> add picture</IonButton> */}
+            <IonAvatar style={{ 'width': '370px', 'height': '370px', }} >\
           <IonItem>
-            <img src={photo}/>
-            <IonFab  vertical="bottom" horizontal="end" >
-            <IonFabButton  onClick={() => this.takePicture()}>
-              <IonIcon icon={add}></IonIcon>
-            </IonFabButton>
-            </IonFab></IonItem>
-          </IonAvatar>
-          <IonItem routerLink = {'/home'}>
-            <IonButton disabled = {this.state.photo===DEFAULT_PIC? true: false} onClick={()=>{window.localStorage.clear()}} type ="submit">done
+                <img src={imageUrl} />
+                <IonFab vertical="bottom" horizontal="end" >
+                  <IonFabButton onClick={() => this.takePicture()}>
+                    <IonIcon icon={add}></IonIcon>
+                  </IonFabButton>
+                </IonFab></IonItem>
+            </IonAvatar>
+            <IonItem routerLink={'/artistpassword'}>
+              <IonButton disabled={this.state.imageUrl === DEFAULT_PIC ? true : false} type="submit">done
             </IonButton>
-          </IonItem>
+            </IonItem>
           </form>
         </IonContent>
       </IonPage >
@@ -83,9 +83,9 @@ export class UploadPicture extends Component <IMyComponentProps,IMyComponentStat
   };
 }
 
-const mapDispatchToProps=dispatch=>{
+const mapDispatchToProps = dispatch => {
   return {
-      updateArtist: (photo) => dispatch(updatedArtist(photo))
+    updateArtist: (artistInfo) => dispatch(updatedArtist(artistInfo))
   }
 }
 export default connect(null, mapDispatchToProps)(UploadPicture);
