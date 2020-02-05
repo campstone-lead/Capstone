4e32e38c29b82c2eb72a77de22e94f63a27ea128
lianach <liana.andreea97@yhaoo.com>
2020-02-05 17:34:59 -0500

    finished artist recommendation page

---
+++ .history/src/pages/Tab1_20200205160401.tsx
@@ -0,0 +1,149 @@
+import {
+  IonCard,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  // IonIcon,
+  // IonItem,
+  // IonLabel,
+  IonItemGroup,
+  // IonListHeader,
+IonButton,
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonSearchbar,
+  IonContent,
+
+} from '@ionic/react';
+
+import React from 'react';
+import {auth,me} from '../store/user'
+import {getRecommendedVenues} from '../store/venue'
+import {getOneBooker} from '../store/booker'
+import {getRocommendedArtists} from '../store/artist'
+import {connect} from 'react-redux'
+import './Tab1.css';
+
+interface IMyComponentState{
+  recommendationsBooker:Array<object>
+}
+interface IMyComponentProps{
+  auth:any,
+  error:any,
+  user:object,
+  me:any,
+  getRecommendedVenues:any,
+  fetchVenues:any,
+  getRocommendedArtists:any,
+  venues:any,
+  artists:any,
+  recommendations:Array<object>
+}
+class Tab1 extends React.Component <IMyComponentProps,IMyComponentState> {
+
+  constructor(props){
+    super(props)
+    this.state={
+      recommendationsBooker:[]
+    }
+  }
+  async componentDidMount(){
+    await this.props.me()
+    if(this.props.user['status']==='booker'){
+      const id=this.props.user['id'];
+      console.log(id)
+      await this.props.fetchVenues(id);
+      this.props.venues.forEach(async(venue,index)  => {
+        await this.props.getRocommendedArtists(venue.id);
+
+        this.props.artists.forEach((artist)=>{
+          console.log(artist.recommendations[0].score)
+          if(artist.recommendations[0].score<10)
+          this.setState({recommendationsBooker:[...this.state.recommendationsBooker,artist]})
+        }
+        )
+      })
+      let sorted=this.state.recommendationsBooker.slice()
+      sorted.sort((a,b)=>a['recommendations'][0].score-b['recommendations'][0].score)
+    }
+
+  }
+
+  render(){
+    console.log( this.state.recommendationsBooker)
+  return (
+    <IonPage>
+       <IonHeader mode="ios"  >
+        <IonToolbar mode="ios" >
+        <div className="tabHeader">
+        <img src="https://www.freepnglogos.com/uploads/music-logo-black-and-white-png-21.png" alt="logo.png" className="logo" />
+        <IonSearchbar
+          mode="ios"
+          className="searchBar"
+          animated
+          showCancelButton="focus"
+          cancelButtonText='x'
+          >
+          </IonSearchbar>
+        </div>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+    {(this.props.user['status']==='booker')?
+
+<div className="home">
+
+<IonCardHeader className="home" mode="ios">
+<IonButton mode="ios"
+href="/artists"
+className="homeBtn" color="rgb(153, 178, 189);">Artists</IonButton>
+<IonCardTitle className="textBox">We got you some artist you might be interested in...</IonCardTitle>
+</IonCardHeader>
+
+{/* this is where artists go */}
+<IonCard className='profile' style={{ "width":"250px"}} mode="ios">
+          <div className='artistBox' >
+            <IonItemGroup>
+              <img  src="https://images.vexels.com/media/users/3/147101/isolated/preview/b4a49d4b864c74bb73de63f080ad7930-instagram-profile-button-by-vexels.png" alt="img.jpg"/>
+            </IonItemGroup>
+            <IonItemGroup>
+            <IonCardTitle>Artist Name</IonCardTitle>
+            <IonCardSubtitle>Genres</IonCardSubtitle>
+            </IonItemGroup>
+          </div>
+        </IonCard>
+
+
+
+</div>
+  :
+
+  <IonCard className="home">
+      <IonCardHeader mode="ios">
+
+      <IonCardTitle className="textBox">{this.props.user['firstName']} {this.props.user['lastName']}</IonCardTitle>
+
+    </IonCardHeader>
+  </IonCard>
+  }
+
+      </IonContent>
+    </IonPage>
+  );
+  }
+};
+const mapStateToProps=(state)=>({
+  error:state.user.error,
+  user:state.user,
+  venues:state.booker.venues,
+  artists:state.artist
+})
+const mapDispatchToProps=(dispatch)=>({
+  auth:(email,password) => dispatch(auth(email,password)),
+  me:()=>dispatch(me()),
+  getRecommendedVenues:(id)=>dispatch(getRecommendedVenues(id)),
+  getRocommendedArtists:(id)=>dispatch(getRocommendedArtists(id)),
+  fetchVenues:(id)=>dispatch(getOneBooker(id))
+})
+export default connect(mapStateToProps,mapDispatchToProps)(Tab1);
---
+++ .history/src/store/venue_20200205140714.ts
@@ -0,0 +1,54 @@
+import axios from 'axios'
+// import history from '../pages/history'
+axios.defaults.withCredentials = true;
+
+
+/**
+ * ACTION TYPES
+ */
+const GET_VENUES = 'GET_VENUES'
+
+
+/**
+ * INITIAL STATE
+ */
+const defaultState = {
+  all: [],
+  selected: {}
+}
+
+/**
+ * ACTION CREATORS
+ */
+const getVenues = venues => ({ type: GET_VENUES, venues })
+
+
+/**
+ * THUNK CREATORS
+ */
+export const fetchVenues = () => async dispatch => {
+  try {
+    const res = await axios({
+      method: "get",
+      baseURL: "http://localhost:8080/api/",
+      url: "/venues/distance/"
+    })
+    window.localStorage.setItem('venueList', JSON.stringify(res.data))
+    dispatch(getVenues(res.data || defaultState))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+
+/**
+ * REDUCER
+ */
+export default function (state = defaultState, action) {
+  switch (action.type) {
+    case GET_VENUES:
+      return { ...state, all: action.venues }
+    default:
+      return state
+  }
+}
