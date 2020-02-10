e562c8be8c39aad5ae23c312d4ef3fe1c43f8f17
Emma Hartman <emmajhartman@gmail.com>
2020-02-10 12:09:39 -0500

    Chore: moved static elements of single venue page into their own component for better modularity

--- src/pages/VenueSinglePage.tsx
+++ src/pages/VenueSinglePage.tsx
@@ -8,6 +8,8 @@ import { fetchEvents, gotOneEvents } from '../store/event'
 import { fetchOneVenue } from '../store/venue';
 import { sendRequest, sendResponse, fetchOneArtists } from '../store/artist';
 import { getOneBooker, getBookerEvents } from '../store/booker'
+
+import VenueSingleComponent from './VenueSingleComponent'
 import history from './history'
 
 interface IMyComponentProps {
@@ -160,14 +162,7 @@ class VenueSinglePage extends React.Component<IMyComponentProps, IMyComponentSta
 
     render() {
         console.log(this.props)
-        let genres = '';
-        if (this.props.venue !== undefined && this.props.venue['genres'] !== undefined) {
-            this.props.venue['genres'].forEach((el, index) => {
-                if (index !== this.props.venue['genres'].length - 1) {
-                    genres += el + ', ';
-                } else genres += el;
-            });
-        }
+
 
         if (!Array.isArray(this.props.events))
             return <IonCardTitle>Loading...</IonCardTitle>;
@@ -198,78 +193,12 @@ class VenueSinglePage extends React.Component<IMyComponentProps, IMyComponentSta
                         className="backBtn"
                     />
                     <div className="profile">
-                        <img src={this.props.venue['imageURL']} alt="img.jpg" />
-                        <IonCardHeader>
-                            <IonCardTitle>{this.props.venue['name']}</IonCardTitle>
-                        </IonCardHeader>
-                        <IonCardContent>
-                            <IonCardSubtitle style={{ "color": "black", "fontSize": "15.5px" }}>{this.props.venue['description']}</IonCardSubtitle>
-                        </IonCardContent>
-
-                        <br></br>
-                        <IonList lines="inset">
-                            <IonItem>
-                                <IonIcon slot="start" color="medium" icon={home} />
-                                <IonLabel style={{ "padding": "5px" }}>  {this.props.venue['address']} </IonLabel>
-                            </IonItem>
-
-                            <IonItem>
-                                <IonIcon slot="start" color="medium" icon={musicalNote} />
-                                <IonLabel style={{ "padding": "5px" }}> {genres} </IonLabel>
-                            </IonItem>
-
-                            <IonItem>
-                                <IonIcon slot="start" color="medium" icon={body} />
-                                <IonLabel style={{ "padding": "5px" }}> Max Capacity: {this.props.venue['capacity']} </IonLabel>
-                            </IonItem>
-                        </IonList>
-                        {this.props.booker['user'] ?
-                            <IonCardTitle>Booker: {this.props.booker["user"]["firstName"]}{' '}{this.props.booker["user"]["lastName"]}</IonCardTitle> : null}
-                        <h1>Upcoming Events:</h1>
-                        {this.props.events ? this.props.events.map((event, index) => {
-                            console.log(typeof event['date'])
-                            var dateObj = new Date(event['date'])
-                            var month = dateObj.getUTCMonth() + 1;
-                            var day = dateObj.getUTCDate();
-                            var year = dateObj.getUTCFullYear();
-
-                            let newdate = year + "/" + month + "/" + day;
-                            return (
-                                <IonCard key={index}
-                                    href={`/events/${event['id']}`}
-                                    className=""
-                                    style={{ width: '250px' }}
-                                    mode="ios"
-                                >
-                                    <div className="eventBox">
-                                        <img src={event['imageURL']} alt="img.jpg" />
-                                        <IonItemGroup style={{ margin: '20px' }}>
-                                            <IonCardTitle
-                                                style={{ textAlign: 'center' }}
-                                                className="eventBox"
-                                            >
-                                                {event['name']}
-                                            </IonCardTitle>
-                                            <IonCardSubtitle
-                                                style={{ textAlign: 'center' }}
-                                            >
-                                                {event['description']}
-                                            </IonCardSubtitle>
-                                            <IonCardSubtitle
-                                                style={{ textAlign: 'center' }}
-                                            >
-                                                {
-
-                                                    newdate
-
-                                                }
-                                            </IonCardSubtitle>
-                                        </IonItemGroup>
-                                    </div>
-                                </IonCard>
-                            )
-                        }) : <h3>This venue currently has no events</h3>}
-
+                        {/* VENUE VIEW WITH LIST OF EVENTS IS HERE NOW!!! */}
+                        <VenueSingleComponent
+                            venue={this.props.venue}
+                            booker={this.props.booker}
+                            events={this.props.events}
+                        />
 
                         <select onChange={this.handleChange}>
                             {this.props.events &&
