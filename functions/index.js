26dc3d851a27ab7f8f0f36a9f54c3e29be64f05b
lianach <liana.andreea97@yhaoo.com>
2020-02-16 19:29:25 -0500

    modified deployed branch for firebase

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,8 @@
 // /* eslint-disable consistent-return */
+
+
+
+//function taht renames duplicates in the cloud storage->can be modified
 // const functions = require('firebase-functions');
 // const { Storage } = require('@google-cloud/storage');
 // let gcs = new Storage({
@@ -56,127 +60,21 @@ const config = {
 const cors = require('cors');
 app.use(cors(config));
 
-
+//not working but can be modified for other purposes!
 app.post('/upload', async (req, res) => {
   (async () => {
     try {
 
       const image = req.body;
+      console.log('parsed imeage', image)
       const metadata = { contentType: 'image/jpeg' }
-      const file
-      console.log('Uploaded!')
     } catch (err) {
       console.log(err)
     }
-    // console.log('Image is here', image)
-    res.json('here')
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send(error);
-  }
-})();
+  })();
 
 });
-// app.get('/meetings', async (req, res) => {
-//   (async () => {
-//     try {
-//       let query = firestore.collection('meetings');
-//       let response = [];
-//       await query.get().then(querySnapshot => {
-//         let docs = querySnapshot.docs;
-//         for (let doc of docs) {
-//           const selectedItem = {
-//             id: doc.id,
-//             data: doc.data()
-//           };
-//           response.push(selectedItem);
-//         }
-//       });
-//       return res.status(200).send(response);
-//     } catch (error) {
-//       console.log(error);
-//       return res.status(500).send(error);
-//     }
-//   })();
-// });
-// app.get('/meetings/:meetingId', async (req, res) => {
-//   (async () => {
-//     try {
-//       const document = firestore.collection('meetings').doc(req.params.meetingId);
-//       let user = await document.get();
-//       let response = user.data();
-//       return res.status(200).send(response);
-//     } catch (error) {
-//       console.log(error);
-//       return res.status(500).send(error);
-//     }
-//   })();
-// });
-// app.get('/users/:userId', async (req, res) => {
-//   (async () => {
-//     try {
-//       const document = await firestore.collection('users').doc(req.params.userId);
-//       let user = await document.get();
-//       let response = user.data();
-//       return res.status(200).send(response);
-//     } catch (error) {
-//       console.log(error);
-//       return res.status(500).send(error);
-//     }
-//   })();
-// });
 
-// app.put('/users/update/:userId', async (req, res) => {
-//   (async () => {
-//     try {
-//       const user = firestore.collection("users").doc(req.params.userId);
-//       await user.update(req.body)
-//       return res.status(200).send()
-//     } catch (err) {
-//       console.log(err)
-//       return res.status(500).send(err)
-//     }
-//   })();
-// })
-
-// app.post('/users/create', (req, res) => {
-//   // const {id,email,firstName,lastName,password}=req.body;
-//   (async () => {
-//     try {
-//       await firestore.collection('users').doc(req.body.email).set(req.body)
-//       return res.status(200).send();
-//     } catch (error) {
-//       console.log(error);
-//       return res.status(500).send(error);
-//     }
-//   })()
-// });
-// app.post('/meetings/create', (req, res) => {
-//   // const {id,email,firstName,lastName,password}=req.body;
-//   (async () => {
-//     try {
-//       const lat = req.body.location.latitude
-//       const lng = req.body.location.longitude
-//       console.log(typeof req.body.date)
-//       await firestore.collection('meetings').doc(req.body.name).set({
-//         name: req.body.name,
-//         description: req.body.description,
-//         location: new admin.firestore.GeoPoint(lat, lng),
-//         date: req.body.date,
-//         user: req.body.user
-//       })
-
-//       const document = await firestore.collection('meetings').doc(req.body.name);
-//       let user = await document.get();
-//       let response = user.data();
-//       return res.status(200).send(response);
-
-//     } catch (error) {
-//       console.log(error);
-//       return res.status(500).send(error);
-//     }
-//   })()
-// });
 
 
 
